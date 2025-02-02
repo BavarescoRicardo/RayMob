90d66cb698ce44ddabc261b03cbb12bf19499f5b
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2025-02-02 17:29:03 -0300

    ajustes velocidades

--- core/src/main/java/io/rba/raymob/Player.java
+++ core/src/main/java/io/rba/raymob/Player.java
@@ -8,8 +8,8 @@ public class Player {
     private Collision collision;
     private Cenario map;
 
-    private final double moveSpeed = 6.0; // Velocidade de movimento (pixels por ciclo)
-    private final double turnSpeed = Math.PI / 20; // Velocidade de rotação (radianos por ciclo)
+    private final double moveSpeed = 1.0; // Velocidade de movimento (pixels por ciclo)
+    private final double turnSpeed = Math.PI / 200; // Velocidade de rotação (radianos por ciclo)
 
     public Player(double x, double y, Cenario map) {
         this.x = x;
